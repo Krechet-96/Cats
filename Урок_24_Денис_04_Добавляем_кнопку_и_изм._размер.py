d25493db1a635025752ada7dbdb377049f0af399
Programmer <shakirov.ekb@gmail.com>
2024-11-16 00:23:19 +0500

    Добавили кнопку для загрузки нового изображения.

---
+++ Урок_24_Денис_04_Добавляем_кнопку_и_изм._размер.py
@@ -0,0 +1,43 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+from pygame.display import update
+
+
+def load_image(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f"Произошла ошибка {e}")
+        return None
+
+
+def set_image():
+    img = load_image(url)
+
+    if img:
+        label.config(image=img)
+        label.image = img
+
+
+window = Tk()
+window.title("Cats!")
+window.geometry("600x480")
+
+label = Label()
+label.pack()
+
+update_button = Button(text="Обновить", command=set_image)
+update_button.pack()
+
+url = "https://cataas.com/cat"
+
+set_image()
+
+window.mainloop()
